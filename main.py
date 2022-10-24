15ab911b1277d3c2bc29d03982cb6cbbe633c9f7
Jack <dev@adwt.ru>
2022-10-24 23:53:47 +0300

    update values names

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-
 import requests
 import json
 from flask import Flask
 
 
-def get_valutes_list():
+def get_currency_list():
     url = 'https://www.cbr-xml-daily.ru/daily_json.js'
     response = requests.get(url)
     data = json.loads(response.text)
-    valutes = list(data['Valute'].values())
-    return valutes
+    currencies = list(data['Valute'].values())
+    return currencies
 
 
 app = Flask(__name__)
 
 
-def create_html(valutes):
+def create_html(currencies):
     text = '<h1>Курс валют</h1>'
     text += '<table>'
     text += '<tr>'
-    for _ in valutes[0]:
+    for _ in currencies[0]:
         text += f'<th><th>'
     text += '</tr>'
-    for valute in valutes:
+    for currency in currencies:
         text += '<tr>'
-        for v in valute.values():
+        for v in currency.values():
             text += f'<td>{v}</td>'
         text += '</tr>'
 
@@ -34,8 +33,8 @@ def create_html(valutes):
 
 @app.route("/")
 def index():
-    valutes = get_valutes_list()
-    html = create_html(valutes)
+    currencies = get_currency_list()
+    html = create_html(currencies)
     return html
 
 
